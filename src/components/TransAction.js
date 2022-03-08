283a19ad4683c357a82b183fb91e679fe09661e2
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-09 00:26:53 +0330

    transaction component

---
+++ src/components/TransAction.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+const TransAction = ({ transactions }) => {
+  const [searchItem, setSearchItem] = useState("");
+  const [filterTnx, setFilterTnx] = useState(transactions);
+
+  const filterTransactions = (search) => {
+    if (!search || search === "") {
+      setFilterTnx(transactions);
+      return;
+    }
+    const filtered = transactions.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilterTnx(filtered);
+  };
+
+  const changeHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [transactions]);
+
+  if (!transactions.length) return <h3>add some tnx ...</h3>;
+
+  return (
+    <section>
+      <input
+        className="search"
+        type="text"
+        value={searchItem}
+        onChange={changeHandler}
+        placeholder="search for tnx ..."
+      />
+      {filterTnx.length
+        ? filterTnx.map((t) => (
+            <div
+              key={t.id}
+              className="transaction"
+              style={{ borderRight: t.type === "expense" && "4px solid red" }}
+            >
+              <span>{t.desc}</span>
+              <span>$ {t.amount}</span>
+            </div>
+          ))
+        : "no item matchs !"}
+    </section>
+  );
+};
+
+export default TransAction;
