4aac6a55be4e22dd4e4f605af12c59d3c81cec72
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-09 00:26:37 +0330

    create overview component

---
+++ src/components/OverView.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import TransActionForm from "./TransActionForm";
+
+const OverView = ({ income, expense, addTransaction }) => {
+  const [isShow, setIsShow] = useState(false);
+
+  return (
+    <>
+      <div className="topSection">
+        <p> Balance : {income - expense}</p>
+        <button
+          onClick={() => setIsShow((prevState) => !prevState)}
+          className={`btn ${isShow && "cancel"}`}
+        >
+          {isShow ? "Cancel" : "Add"}
+        </button>
+      </div>
+      {isShow && (
+        <TransActionForm
+          addTransaction={addTransaction}
+          setIsShow={setIsShow}
+        />
+      )}
+      <div className="resultSection">
+        <div className="expenseBox">
+          Expense <span style={{ color: "red" }}>{expense} $</span>
+        </div>
+        <div className="expenseBox">
+          Income <span>{income} $</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OverView;
